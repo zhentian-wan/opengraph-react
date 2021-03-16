d78fdc52a62fea3385052da8b5f507b68e76fd02
Zhentian Wan <zhentian.wan@varian.com>
2021-03-16 14:05:39 +0200

    fix build

---
+++ build/precache-manifest.9c3ac4235a6037d7a9ebce5f044aded7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f42066f76007e40fe36525091c8e49da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a59beabd8266d90b5fcf",
+    "url": "/static/js/2.e32d4e53.chunk.js"
+  },
+  {
+    "revision": "0d2b93928adb90ea14d7",
+    "url": "/static/js/main.3a8c22a6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
